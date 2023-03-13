df8dd8d646bcc5f58872dd5507a37732718a8de8
esatgundogdu <esatgundogdu01@gmail.com>
2023-03-14 01:18:55 +0300

    zsh complete from history

--- zsh/bindings.zsh
+++ zsh/bindings.zsh
@@ -10,3 +10,11 @@ bindkey  "^[[F"   end-of-line
 
 bindkey "^D" exit
 
+# search from history with up and down keys
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
+
